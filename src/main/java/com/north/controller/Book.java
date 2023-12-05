80f5fb2880c554054b283b0d1a33ca302dacaf77
NorthPoet <2957578427@qq.com>
2023-12-05 16:12:52 +0800

    two commit

---
+++ src/main/java/com/north/controller/Book.java
@@ -0,0 +1,15 @@
+package com.north.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author North
+ * @Date 2023/12/5 16:11
+ */
+@RestController
+public class Book {
+
+    public String test() {
+        return "Hello Git";
+    }
+}
